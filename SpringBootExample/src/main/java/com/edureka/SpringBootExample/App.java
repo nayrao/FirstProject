89eddf1ec6f3d4a113a065e783af7cabe8398c69
Admin <Admin@Admin-PC>
2019-11-18 21:26:55 +0530

    firsttimecommit

---
+++ SpringBootExample/src/main/java/com/edureka/SpringBootExample/App.java
@@ -0,0 +1,18 @@
+package com.edureka.SpringBootExample;
+
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootConfiguration
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        
+    }
+}
