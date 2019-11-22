5ceb8d02893bbd19fd6fdb1b36473e30d69f3180
Admin <Admin@Admin-PC>
2019-11-22 18:52:40 +0530

    added new annotaion

--- SpringBootExample/src/main/java/com/edureka/SpringBootExample/App.java
+++ SpringBootExample/src/main/java/com/edureka/SpringBootExample/App.java
@@ -1,12 +1,14 @@
 package com.edureka.SpringBootExample;
 
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
 @SpringBootConfiguration
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
